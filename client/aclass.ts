170e9258933d845686085e92f2ce9c8ed1e7670a
Qunqun Xu <qunqun.xu@autodesk.com>
2021-03-08 15:16:15 +0800

    Test class & namespace definitions

---
+++ client/aclass.ts
@@ -0,0 +1,6 @@
+// https://developerhandbook.com/typescript/writing-angularjs-1-x-with-typescript/#modules
+
+class AClass {
+    "use strict"
+}
+
---
+++ client/anamespace.ts
@@ -0,0 +1,9 @@
+// https://developerhandbook.com/typescript/writing-angularjs-1-x-with-typescript/#modules
+
+namespace ANamespace {
+    "use strict"
+
+    class AClass {
+    }
+}
+
