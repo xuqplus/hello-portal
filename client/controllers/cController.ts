33cff47c6b4673fcb526716e0159a475dada1861
Qunqun Xu <qunqun.xu@autodesk.com>
2021-03-09 14:26:51 +0800

    Angularjs two-way data binding - $scope

--- client/controllers/cController.ts
+++ client/controllers/cController.ts
@@ -1,17 +1,21 @@
 namespace bpp.controllers {
 
-    import AService = bpp.services.AService;
-
     class CController implements ng.IComponentController {
-        private aService: AService; // for grunt ts compilation
+        private aService: bpp.services.AService; // for grunt ts compilation
+        private $scope: ng.IScope;
+        private $rootScope: ng.IRootScopeService;
+        private cModel: string;
 
-        constructor(aService: bpp.services.AService) {
-            // auto injections
-            this.aService = aService; // for grunt ts compilation
+        constructor($scope: ng.IScope,/* $rootScope: ng.IRootScopeService,*/ aService: bpp.services.AService) {
+            this.$scope = $scope; // for grunt ts compilation
+            // this.$rootScope = $rootScope;
+            this.aService = aService;
+            this.cModel = 'cModel'
         }
 
         cClick: Function = (): void => {
             console.log('cClick');
+            console.log(this);
             // aService.a(); // will end up in 'grunt ts' compilation error, but actually this can also work
             this.aService.a(); // for grunt ts compilation
         }
