cc913dbdf68004b8b33db7fb97328b5cfbe2095e
Qunqun Xu <qunqun.xu@autodesk.com>
2021-03-09 13:49:27 +0800

    Fix grunt ts error

--- client/controllers/cController.ts
+++ client/controllers/cController.ts
@@ -1,13 +1,19 @@
 namespace bpp.controllers {
 
+    import AService = bpp.services.AService;
+
     class CController implements ng.IComponentController {
+        private aService: AService; // for grunt ts compilation
+
         constructor(aService: bpp.services.AService) {
             // auto injections
+            this.aService = aService; // for grunt ts compilation
         }
 
         cClick: Function = (): void => {
             console.log('cClick');
-            aService.a();
+            // aService.a(); // will end up in 'grunt ts' compilation error, but actually this can also work
+            this.aService.a(); // for grunt ts compilation
         }
     }
 
